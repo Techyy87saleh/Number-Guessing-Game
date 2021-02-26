2eb576cee1805bf839d7d4e38adb57de6fc5efdf
Techyy87saleh <79599447+Techyy87saleh@users.noreply.github.com>
2021-02-26 14:25:33 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+     let para = document.getElementById('content');
+    let input = document.getElementById('form').value;
+    let button = document.getElementById('btn'); 
+
+    button.addEventListener('click',(e)=>{
+     
+    e.preventDefault()
+
+     let randomNumber = (Math.floor(Math.random()*100));
+
+     if(input === randomNumber){
+         para.innerHTML = "Your Number is  high";
+     }
+     else if(input < randomNumber){
+         para.innerHTML  = "Your Number is too low";
+     }
+     else if(input > randomNumber){
+         para.innerHTML  = "Your number is too high"
+     }
+     else{
+         para.innerHTML  = "Not Good,try again!"
+     }
+});
